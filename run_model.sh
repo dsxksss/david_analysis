aa9b5afce1233e6fc56a3cee9ca92e1af7258084
VentCat <2546650292@qq.com>
2024-05-24 16:59:45 +0800

    调整脚本执行命令

--- run_model.sh
+++ run_model.sh
@@ -1,2 +1,4 @@
-./.venv/Scripts/python.exe ./src/david_analysis --input test.txt --auth_email 914086859@qq.com
-# /data/PRG/tools/TCM/apps/david_analysis/.venv/Scripts/python.exe /data/PRG/tools/TCM/apps/david_analysis/src/david_analysis --input test.txt --auth_email 914086859@qq.com
+# 当前路径运行命令
+# ./.venv/bin/python ./src/david_analysis --input test.txt --auth_email 914086859@qq.com
+# 绝对路径运行命令
+# /data/PRG/tools/TCM/apps/david_analysis/.venv/bin/python /data/PRG/tools/TCM/apps/david_analysis/src/david_analysis --input test.txt --auth_email 914086859@qq.com
